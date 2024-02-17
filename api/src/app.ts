696e2321a8b59b45e431a21ff2629149822fa27b
Tanel Torn <tanel.torn@gmail.com>
2024-02-17 09:21:44 +0200

    wip

--- api/src/app.ts
+++ api/src/app.ts
@@ -17,8 +17,23 @@ const logger = log4js.getLogger();
 logger.level = process.env.LOG_LEVEL;
 
 logger.info(`Log level: ${process.env.LOG_LEVEL}`)
+logger.info(`Allowed origin: ${process.env.ALLOWED_ORIGIN}`)
 
+// todo seems this is not supported:
 // const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || ["http://localhost"];
+
+// todo try this instead
+// var allowedOrigins = ['http://localhost:3000',
+//     'http://yourapp.com'];app.use(cors({
+//     origin: function(origin, callback){    // allow requests with no origin
+//         // (like mobile apps or curl requests)
+//         if(!origin) return callback(null, true);    if(allowedOrigins.indexOf(origin) === -1){
+//             var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }    return callback(null, true);
+//     }
+// }));
 
 app.use(cors({
     credentials: true,
