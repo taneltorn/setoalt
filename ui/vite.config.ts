326c4a5a945a8866d047f00581e0ea7c586b2dce
Tanel Torn <tanel.torn@gmail.com>
2024-12-12 11:03:47 +0200

    experiment 2

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/setonoot/',
   server: {
     host: true,
     port: 5173
   },
+  base: './',
   plugins: [react()],
   envDir: '../',
 })
